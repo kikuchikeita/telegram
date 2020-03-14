d2268a5c3ff8b4013836a29e89650c473b338357
kikuchikeita <imnotpooh@gmail.com>
2020-03-14 16:54:46 +0900

    add アソシエーション

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,8 @@ class PostsController < ApplicationController
     @post = Post.new # フォーム用の空のインスタンスを生成する。
   end
 
-  def create
-    @post = Post.new(post_params) # ストロングパラメータを引数に
+  def create    
+    @post = current_user.posts.new(post_params)
     if @post.save # saveをしてデータベースに保存する。
       redirect_to @post, notice: '投稿を保存しました' # showページにリダイレクト
     else
