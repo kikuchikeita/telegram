6318cf025db68fd0b38afb6bf4416d0a84aeaa27
kikuchikeita <imnotpooh@gmail.com>
2020-03-14 14:06:25 +0900

    add delte modal

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,6 +33,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "投稿を削除しました。"
+  end
+
+
 
   private
 
