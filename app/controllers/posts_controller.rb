bac405faaa4fc48d410fc9392a6798127aee7835
kikuchikeita <imnotpooh@gmail.com>
2020-03-15 15:34:41 +0900

    add 検索

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [ :edit, :update, :destroy]
   def index
-    @posts = Post.find_newest_post(params[:page]).with_user_and_comment
     @comment = Comment.new
   end
 
